dd085f2d66a60803dedcbf15464d1f524f6302e6
ppuskari <ppuskari@hotmail.com>
2026-09-19 13:05:46 -0500

    Fix dispatch scanner pattern

--- tools/inspect_printshop_source.py
+++ tools/inspect_printshop_source.py
@@ -32,7 +32,8 @@ IMAGE_SIZE = TRACKS * SECTORS * SECTOR_SIZE
 
 TERMS = re.compile(
     r"(printer|print[.]|imagewriter|epson|okidata|okig|microline|driver|graphics|raster|"
-    r"bit.?image|parallel|serial|interface|pr#|sendgc|gcout|sendrow|lfcr|line.feed|"\n    r"prparams|crlf|sgc[0-9a-z]*|gc[0-9][a-z]*|okidata|okig)",
+    r"bit.?image|parallel|serial|interface|pr#|sendgc|gcout|sendrow|lfcr|line.feed|"
+    r"prparams|crlf|sgc[0-9a-z]*|gc[0-9][a-z]*)",
     re.IGNORECASE,
 )
 
