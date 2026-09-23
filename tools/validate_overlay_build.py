4a3e600b27c8bacee0c0aaf14142a5be8d9cc727
ppuskari <ppuskari@hotmail.com>
2026-09-23 02:20:00 -0500

    R27: add stationery overlay discovery validation

--- tools/validate_overlay_build.py
+++ tools/validate_overlay_build.py
@@ -14,6 +14,8 @@ EXPECTED = {
     "MENUS7.OKI":  (3018, "1562e1ad72c5660ade0ccda7ef9cfa439805ee35e96fc3a5a923096c7d37d485"),
     "GCDRAW.ORIG": (2737, "cfa548eb4f950156c14639372f2681edbaa810e86f0945d73c24e0304e436353"),
     "GCDRAW.OKI":  (2810, "4bd76c9d9fbe1a32870b00edc3ca54061037dc6cd4491eb8202b5e7f26c9db4a"),
+    "LHDRAW.ORIG": (None, None),  # R27 discovery
+    "LHDRAW.OKI":  (None, None),  # R27 discovery
 }
 
 
